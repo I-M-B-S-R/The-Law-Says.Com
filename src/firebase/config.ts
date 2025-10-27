a136c643b1a3958927ffdf73231f674ae45c81ed
Brian Reynolds <t.a.l.q.r.m.a@gmail.com>
2025-10-27 23:37:27 +0000

    File changes

---
+++ src/firebase/config.ts
@@ -0,0 +1,12 @@
+
+// This configuration is public and can be safely Githared.
+// It's used to identify your app to the Firebase backend.
+// Security is enforced by Firebase Security Rules, not by hiding this object.
+export const firebaseConfig = {
+  projectId: "studio-9218455660-be898",
+  appId: "1:322149840321:web:e40016c4a08a4e203cab40",
+  apiKey: "AIzaSyCiOQsqWqNYwPitetoBUz6ubN003PqI0UI",
+  authDomain: "studio-9218455660-be898.firebaseapp.com",
+  measurementId: "",
+  messagingSenderId: "322149840321",
+};
