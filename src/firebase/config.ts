f06aae081496bad4aef57814f04550fe8e5d55b8
Brian Reynolds <t.a.l.q.r.m.a@gmail.com>
2025-10-28 05:12:29 +0000

    My Mistake, I Had That File But It Was A .js , So I Made It A .tsx And U

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,4 +1,3 @@
-
 // This configuration is public and can be safely shared.
 // It's used to identify your app to the Firebase backend.
 // Security is enforced by Firebase Security Rules, not by hiding this object.
