cc7ea7f030e1b0e738d2b20222d2f9c3a066bc0f
Brian Reynolds <t.a.l.q.r.m.a@gmail.com>
2025-10-28 04:15:18 +0000

    I Switched To This Project From The Other 1 Above It.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,12 @@
+
 // This configuration is public and can be safely Githared.
 // It's used to identify your app to the Firebase backend.
 // Security is enforced by Firebase Security Rules, not by hiding this object.
 export const firebaseConfig = {
   apiKey: "AIzaSyCiOQsqWqNYwPitetoBUz6ubN003PqI0UI",
-  authDomain: "talqrma-26.firebaseapp.com",
-  projectId: "talqrma-26",
-  storageBucket: "talqrma-26.appspot.com",
+  authDomain: "studio-9218455660-be898.firebaseapp.com",
+  projectId: "studio-9218455660-be898",
+  storageBucket: "studio-9218455660-be898.appspot.com",
   messagingSenderId: "322149840321",
   appId: "1:322149840321:web:87dfddccff8b46713cab40"
 };
