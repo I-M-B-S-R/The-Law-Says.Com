06ab2531e52b1fa54c25def610192615ab2a3ac4
Brian Reynolds <t.a.l.q.r.m.a@gmail.com>
2025-10-28 00:49:34 +0000

    PS C:\projects\The-Law-Says.Com> firebase deploy
    === Deploying to 'talq

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,9 +4,9 @@
 // Security is enforced by Firebase Security Rules, not by hiding this object.
 export const firebaseConfig = {
   apiKey: "AIzaSyCiOQsqWqNYwPitetoBUz6ubN003PqI0UI",
-  authDomain: "studio-9218455660-be898.firebaseapp.com",
-  projectId: "studio-9218455660-be898",
-  storageBucket: "studio-9218455660-be898.firebasestorage.app",
+  authDomain: "talqrma-26.firebaseapp.com",
+  projectId: "talqrma-26",
+  storageBucket: "talqrma-26.appspot.com",
   messagingSenderId: "322149840321",
   appId: "1:322149840321:web:87dfddccff8b46713cab40"
 };
