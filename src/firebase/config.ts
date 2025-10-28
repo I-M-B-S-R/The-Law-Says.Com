3ea0b7319404057f8384cdf19ed96c670bd9e635
Brian Reynolds <t.a.l.q.r.m.a@gmail.com>
2025-10-28 04:19:43 +0000

    PS C:\projects\The-Law-Says.Com> git add src/firebase/config.ts src/cont

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,4 +1,3 @@
-
 // This configuration is public and can be safely Githared.
 // It's used to identify your app to the Firebase backend.
 // Security is enforced by Firebase Security Rules, not by hiding this object.
