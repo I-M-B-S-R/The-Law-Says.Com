2335dde09e8490fc360bd1f591d3a2cb07f9539d
Brian Reynolds <t.a.l.q.r.m.a@gmail.com>
2025-10-28 00:36:59 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,10 +3,10 @@
 // It's used to identify your app to the Firebase backend.
 // Security is enforced by Firebase Security Rules, not by hiding this object.
 export const firebaseConfig = {
-  projectId: "studio-9218455660-be898",
-  appId: "1:322149840321:web:e40016c4a08a4e203cab40",
   apiKey: "AIzaSyCiOQsqWqNYwPitetoBUz6ubN003PqI0UI",
   authDomain: "studio-9218455660-be898.firebaseapp.com",
-  measurementId: "",
+  projectId: "studio-9218455660-be898",
+  storageBucket: "studio-9218455660-be898.firebasestorage.app",
   messagingSenderId: "322149840321",
+  appId: "1:322149840321:web:87dfddccff8b46713cab40"
 };
